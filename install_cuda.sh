9ec5de9f59d3098f3c7e337c887969ccdb5e19d4
Ying Huang <yhuang@imvu.com>
2017-10-07 23:42:59 -0700

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,33 +3,31 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ ! -f "./installer/cuda_9.0.176_384.81_linux.run" ]
-then
-    wget "https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run " -O "./installer/cuda_9.0.176_384.81_linux.run"
-fi
 
-#apt-get install -y \
-    #    build-essential \
-    #    build-essential \
-    #    freeglut3-dev \
-    #    freeglut3-dev \
-    #    libgl1-mesa-dev \
-    #    libgl1-mesa-glx \
-    #    libglu1-mesa \
-    #    libglu1-mesa-dev \
-    #    libglu1-mesa-dev \
-    #    libx11-dev \
-    #    libxi-dev \
-    #    libxi-dev \
-    #    libxmu-dev \
-    #    libxmu-dev \
-    #    nvidia-cuda-toolkit
-#sh ./installer/cuda_9.0.176_384.81_linux.run --silent --toolkit --samples --samplespath=/media/volgrp --tmpdir=/media/volgrp/temp/
+apt-get install -y \
+    build-essential \
+    build-essential \
+    freeglut3-dev \
+    freeglut3-dev \
+    libgl1-mesa-dev \
+    libgl1-mesa-glx \
+    libglu1-mesa \
+    libglu1-mesa-dev \
+    libglu1-mesa-dev \
+    libx11-dev \
+    libxi-dev \
+    libxi-dev \
+    libxmu-dev \
+    libxmu-dev \
+    nvidia-cuda-toolkit
+rm -fr /media/volgrp/NVIDIA_CUDA-*
+/usr/local/cuda/bin/uninstall_cuda_*.pl
+sh ./installer/cuda_9.0.176_384.81_linux.run --silent --toolkit --samples --samplespath=/media/volgrp --tmpdir=/media/volgrp/temp/
 cp ./etc/profile /etc/profile \
     && source /etc/profile
 cp ./etc/ld.so.conf.d/cuda.conf  /etc/ld.so.conf.d/cuda.conf \
     && ldconfig
 
 cd /media/volgrp/NVIDIA_CUDA-9.0_Samples/ \
-    && make -j \
-    && ./bin/x86_64/linux/release/deviceQuery
+    && make -j
+./bin/x86_64/linux/release/deviceQuery

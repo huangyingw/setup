5bbf66e8505579c6b15b9f90eabd74199b390227
Ying Huang <yhuang@imvu.com>
2017-10-07 18:01:04 -0700

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,10 +3,10 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ ! -f "$SCRIPTPATH/installer/cuda_9.0.176_384.81_linux.run" ]
+if [ ! -f "./installer/cuda_9.0.176_384.81_linux.run" ]
 then
     wget "https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run " -O "./installer/cuda_9.0.176_384.81_linux.run"
 fi
 
 apt-get install -y nvidia-cuda-toolkit libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev freeglut3-dev build-essential libx11-dev libxmu-dev libgl1-mesa-dev libglu1-mesa libglu1-mesa-dev libxi-dev
-sh $SCRIPTPATH/installer/cuda_9.0.176_384.81_linux.run --silent --toolkit --samples --samplespath=/media/volgrp --tmpdir=/media/volgrp/temp/
+sh ./installer/cuda_9.0.176_384.81_linux.run --silent --toolkit --samples --samplespath=/media/volgrp --tmpdir=/media/volgrp/temp/

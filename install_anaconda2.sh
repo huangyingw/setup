e594f8de2f79cb9041daa551e8511b5208aa2c9d
Ying Huang <yhuang@imvu.com>
2017-10-08 23:39:15 -0700

    n

--- install_anaconda2.sh
+++ install_anaconda2.sh
@@ -7,7 +7,10 @@ if [ ! -d /media/volgrp/anaconda2  ]
 then
     bash "./installer/Anaconda2-5.0.0.1-Linux-x86_64.sh" -b -p /media/volgrp/anaconda2
 fi
-cp -v ./.bashrc ~/.bashrc
-source ~/.bashrc
+cp -v ./.bashrc /root/.bashrc
+export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
+export CUDA_HOME=/usr/local/cuda
+# added by Anaconda2 4.4.0 installer
+export PATH="/media/volgrp/anaconda2/bin:$PATH"
 conda install -y bcolz
 conda upgrade -y --all
--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#./install_basic.sh
-#./install_driver.sh
-#./install_cuda.sh
-./install_anaconda2.sh
-./install_theano.sh
-./install_keras.sh
-./install_cudnn.sh
-./install_jupyter.sh
+#. ./install_basic.sh
+#. ./install_driver.sh
+#. ./install_cuda.sh
+. ./install_anaconda2.sh
+. ./install_theano.sh
+. ./install_keras.sh
+. ./install_cudnn.sh
+. ./install_jupyter.sh

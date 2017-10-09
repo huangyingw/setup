2b8a015879e8b4484431005f17783a9fdffd2228
Ying Huang <yhuang@imvu.com>
2017-10-08 23:55:45 -0700

    n

--- install_anaconda2.sh
+++ install_anaconda2.sh
@@ -5,9 +5,10 @@ cd "$SCRIPTPATH"
 
 if [ ! -d /media/volgrp/anaconda2  ]
 then
-    bash "./installer/Anaconda2-5.0.0.1-Linux-x86_64.sh" -u -b -p /media/volgrp/anaconda2
+    bash "./installer/Anaconda2-4.4.0-Linux-x86_64.sh" -u -b -p /media/volgrp/anaconda2
 fi
 cp -v ./.bashrc /root/.bashrc
+source /root/.bashrc 
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
 export CUDA_HOME=/usr/local/cuda
 # added by Anaconda2 4.4.0 installer

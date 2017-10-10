d56212f898e6d6a75feb759d97ae3d3684c310f7
Ying Huang <yhuang@imvu.com>
2017-10-09 23:16:03 -0700

    n

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -6,6 +6,7 @@ cd "$SCRIPTPATH"
 # configure jupyter and prompt for password
 jupyter notebook --generate-config
 cp -v ./.jupyter/jupyter_notebook_config.py  $HOME/.jupyter/jupyter_notebook_config.py
+cp -v ./.jupyter/jupyter_notebook_config.json  $HOME/.jupyter/jupyter_notebook_config.json
 
 echo "\"jupyter notebook\" will start Jupyter on port 8888"
 echo "If you get an error instead, try restarting your session so your $PATH is updated"

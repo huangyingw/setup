2eec7b138b5c752e6dfcde10fb42592b8ccde37d
Ying Huang <yhuang@imvu.com>
2017-10-10 19:15:30 -0700

    n

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -5,6 +5,7 @@ cd "$SCRIPTPATH"
 
 # configure jupyter and prompt for password
 jupyter notebook --generate-config
-cp -v ./.jupyter/jupyter_notebook_config.py  $HOME/.jupyter/jupyter_notebook_config.py
-cp -v ./.jupyter/jupyter_notebook_config.json  $HOME/.jupyter/jupyter_notebook_config.json
-cp -v ./.jupyter/custom/custom.js   $HOME/.jupyter/custom/custom.js
+cp -v ./.jupyter/jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
+cp -v ./.jupyter/jupyter_notebook_config.json $HOME/.jupyter/jupyter_notebook_config.json
+mkdir -p $HOME/.jupyter/custom/
+cp -v ./.jupyter/custom/custom.js $HOME/.jupyter/custom/custom.js

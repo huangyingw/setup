fd9d2dbf8cc8502fc512f21d967c5fa5a6a4d96e
Ying Huang <yhuang@imvu.com>
2017-11-03 17:55:25 -0700

    n

--- install_anaconda2.sh
+++ install_anaconda2.sh
@@ -7,7 +7,6 @@ if [ ! -d /media/volgrp/anaconda2  ]
 then
     bash "./installer/Anaconda2-5.0.0.1-Linux-x86_64.sh" -u -b -p /media/volgrp/anaconda2
 fi
-cp -v ./.bashrc /root/.bashrc
 source /root/.bashrc
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
 export CUDA_HOME=/usr/local/cuda
--- install_theano.sh
+++ install_theano.sh
@@ -5,6 +5,7 @@ cd "$SCRIPTPATH"
 
 apt-get install -y python-pip python-dev build-essential
 pip install --upgrade pip
+pip install kaggle-cli
 pip install --upgrade virtualenv
 conda install -y theano
 cp ./.theanorc ~/.theanorc

d962080115a57f6278c2e7c7c778c89bd0c2e220
Ying Huang <yhuang@imvu.com>
2017-11-20 20:01:49 -0800

    n

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -1,6 +1,7 @@
-#!/bin/bash - 
+#!/bin/bash -
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-pip install tensorflow-gpu
+pip install --ignore-installed --upgrade \
+    https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.4.0-py2-none-any.whl

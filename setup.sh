35a62ea5f90a94473f65e8eee1faecb7cae59106
Ying Huang <yhuang@imvu.com>
2017-11-20 19:21:52 -0800

    n

--- setup.sh
+++ setup.sh
@@ -10,8 +10,9 @@ cd "$SCRIPTPATH"
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
 export CUDA_HOME=/usr/local/cuda
 export PATH="/media/volgrp/anaconda2/bin:$PATH"
-. ./install_theano.sh
-. ./install_keras.sh
-. ./install_cudnn.sh
-. ./install_jupyter.sh
+#. ./install_theano.sh
+#. ./install_keras.sh
+. ./install_tensorflow.sh
+#. ./install_cudnn.sh
+#. ./install_jupyter.sh
 #. ./verify_cuda.sh

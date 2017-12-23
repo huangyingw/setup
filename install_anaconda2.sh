d76bca71fae58ed5bdbe6e7bceca4a1826b8cf4b
Ying Huang <yhuang@imvu.com>
2017-12-22 16:48:58 -0800

    n

--- install_anaconda2.sh
+++ install_anaconda2.sh
@@ -3,11 +3,13 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-bash "./installer/Anaconda2-5.0.1-Linux-x86_64.sh" -u -b -p /media/volgrp/anaconda2
-source /root/.bashrc
+realAnaconda="/media/volgrp/anaconda2.5"
+~/loadrc/bashrc/mklnk.sh "$HOME/anaconda2" "$realAnaconda"
+bash "./installer/Anaconda2-5.0.1-Linux-x86_64.sh" -u -b
+source $HOME/.bashrc
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
 export CUDA_HOME=/usr/local/cuda
-export PATH="/media/volgrp/anaconda2/bin:$PATH"
+export PATH="$HOME/anaconda2/bin:$PATH"
 conda install -y bcolz
 conda install -y -c menpo ffmpeg
 conda upgrade -y --all
--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#. ./install_basic.sh
-#. ./install_driver.sh
-#. ./install_cuda.sh
-#. ./install_anaconda2.sh
+. ./install_basic.sh
+. ./install_driver.sh
+. ./install_cuda.sh
+. ./install_anaconda2.sh
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
 export CUDA_HOME=/usr/local/cuda
-export PATH="/media/volgrp/anaconda2/bin:$PATH"
-#. ./install_theano.sh
-#. ./install_keras.sh
+export PATH="$HOME/anaconda2/bin:$PATH"
+. ./install_theano.sh
+. ./install_keras.sh
 . ./install_pytorch.sh
-#. ./install_cudnn.sh
-#. ./install_jupyter.sh
-#. ./verify_cuda.sh
+. ./install_cudnn.sh
+. ./install_jupyter.sh
+. ./verify_cuda.sh

f5ee2b92b02bab6cfff904195ad8bc8d77a010ea
Ying Huang <yhuang@imvu.com>
2017-12-23 22:48:22 -0800

    n

--- install_driver.sh
+++ install_driver.sh
@@ -4,6 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 cp -fv ./etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf
+cp -fv ./usr/NX/etc/node.cfg /usr/NX/etc/node.cfg
 add-apt-repository ppa:graphics-drivers/ppa -y
 apt-get update
 apt-get -y install $(ubuntu-drivers devices|awk '/recommended/{print $3}')

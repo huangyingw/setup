c9f7e403f8007fd2759e6c3cb83ea49ffb79bc4d
Ying Huang <yhuang@imvu.com>
2017-12-23 22:47:00 -0800

    n

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -5,10 +5,7 @@ cd "$SCRIPTPATH"
 
 # configure jupyter and prompt for password
 jupyter notebook --generate-config
-jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
-echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
-
-echo "\"jupyter notebook\" will start Jupyter on port 8888"
-echo "If you get an error instead, try restarting your session so your $PATH is updated"
+cp -v ./.jupyter/jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
+cp -v ./.jupyter/jupyter_notebook_config.json $HOME/.jupyter/jupyter_notebook_config.json
+mkdir -p $HOME/.jupyter/custom/
+cp -v ./.jupyter/custom/custom.js $HOME/.jupyter/custom/custom.js

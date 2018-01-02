398df20226f9cd0c9424edfd91eba545b09aba71
Ying Huang <yhuang@imvu.com>
2018-01-02 10:33:02 -0800

    n

--- install_others.sh
+++ install_others.sh
@@ -3,6 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-pip install pathlib fastai
+pip install pathlib fastai gpustat
 conda install -y bcolz
 conda install -y -c menpo ffmpeg

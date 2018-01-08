97fc2f6467b3c907484d728167d685f856a99cb1
Ying Huang <yhuang@imvu.com>
2018-01-07 23:55:30 -0800

    n

---
+++ install_tensorflow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+pip install --ignore-installed --upgrade \
+    https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.0-cp27-none-linux_x86_64.whl

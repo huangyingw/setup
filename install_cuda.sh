d25f6ba21f8a79fd485203b4abd6f8496579bb1f
Ying Huang <yhuang@imvu.com>
2018-01-08 23:33:51 -0800

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -6,4 +6,4 @@ cd "$SCRIPTPATH"
 dpkg -i ./installer/cuda-repo-ubuntu1704-9-1-local_9.1.85-1_amd64.deb
 apt-key add /var/cuda-repo-<version>/7fa2af80.pub
 apt-get update
-apt-get install cuda
+apt-get install -y cuda

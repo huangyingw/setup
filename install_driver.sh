b468653fc3b3fe99916522707c600a77a1277332
Ying Huang <yhuang@imvu.com>
2018-01-08 18:01:14 -0800

    n

--- install_driver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-cp -fv ./etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf
-add-apt-repository ppa:graphics-drivers/ppa -y
-apt-get update
-apt-get -y install $(ubuntu-drivers devices|awk '/recommended/{print $3}')

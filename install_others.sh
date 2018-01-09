bdc8bcd22dd4f39e2150e5c9ddb68c6970b3a59a
Ying Huang <yhuang@imvu.com>
2018-01-08 18:19:29 -0800

    n

--- install_others.sh
+++ install_others.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -
 
 pip install pathlib fastai gpustat
+export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
+export CUDA_HOME=/usr/local/cuda
+export PATH="$HOME/anaconda3/bin:$PATH"
 conda install -y bcolz
 conda install -y -c menpo ffmpeg

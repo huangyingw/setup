189d1472fa2f3cdcedf2995403e57ae1d59518aa
Ying Huang <yhuang@imvu.com>
2018-01-09 17:51:00 -0800

    n

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -3,5 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl
-pip3 install torchvision
+conda install pytorch torchvision cuda90 -c pytorch

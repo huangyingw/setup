fc0b55a7298bb1c3a1a8ea181f1dc530040157f0
Ying Huang <yhuang@imvu.com>
2018-01-10 18:41:15 -0800

    n

--- install_keras.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-# install and configure keras
-pip3 install keras
-mkdir ~/.keras
-cp -v ./.keras/keras.json  ~/.keras/keras.json
--- install_pytorch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-conda install pytorch torchvision cuda90 -c pytorch

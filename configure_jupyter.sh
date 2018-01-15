ea6bdeddb9e534aa106301779dd5ec8bf86c5c61
Ying Huang <yhuang@imvu.com>
2018-01-15 14:35:49 -0800

    n

---
+++ configure_jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+# configure jupyter and prompt for password
+cp -v ./.jupyter/jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
+cp -v ./.jupyter/jupyter_notebook_config.json $HOME/.jupyter/jupyter_notebook_config.json
+mkdir -p $HOME/.jupyter/custom/
+cp -v ./.jupyter/custom/custom.js $HOME/.jupyter/custom/custom.js

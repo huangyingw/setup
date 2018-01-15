2a2db1bfa2f2e8848c1bcdb8de49e9c8792553b8
Ying Huang <yhuang@imvu.com>
2018-01-15 14:28:01 -0800

    n

--- install_others.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-pip install pathlib fastai gpustat
-conda install -y bcolz
-conda install -y -c menpo ffmpeg
--- install_tensorflow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
-
-apt-get update && apt-get install -y python-pip python-dev build-essential
-pip install --upgrade pip
-pip install --upgrade virtualenv
-pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.0-cp27-none-linux_x86_64.whl

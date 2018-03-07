76d1464fe5a1e7e94913bd7efdbae636383a0b20
Ying Huang <yhuang@imvu.com>
2018-03-06 21:45:50 -0800

    n

---
+++ install_dl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+./install_gpu.sh
+./install_cuda.sh
+./install_cudnn.sh
--- setup.sh
+++ setup.sh
@@ -4,9 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 . ./install_basic.sh
-#. ./install_gpu.sh
-#. ./install_cuda.sh
-#. ./install_cudnn.sh
+#. ./install_dl.sh
 . ./install_anaconda3.sh
 export CUDA_HOME=/usr/local/cuda
 export PATH="$HOME/anaconda3/bin:$PATH"

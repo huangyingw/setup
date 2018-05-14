54d2b09e6c308f388252cb1f568f52cd0db3300a
Ying Huang <yhuang@imvu.com>
2018-05-13 17:13:00 -0700

    n

--- install_anaconda3.sh
+++ install_anaconda3.sh
@@ -3,4 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-bash ./installer/Anaconda3-5.0.1-Linux-x86_64.sh -u -b
+# Install Anaconda3
+RUN curl -so ~/anaconda3.sh https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh \
+    && chmod +x ~/anaconda3.sh \
+    && ~/anaconda3.sh -b -p ~/anaconda3 \
+    && rm ~/anaconda3.sh

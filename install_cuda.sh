d165ea5d369dd87e3d3b19554e7ec982136abc0c
Ying Huang <yhuang@imvu.com>
2018-05-16 21:50:12 -0700

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,7 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-dpkg -i ./installer/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64.deb
+dpkg -i /media/volgrp/software/AI/installer/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64.deb
 apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub
 apt-get update
 apt-get install -y cuda \

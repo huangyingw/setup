50cff210cd01fabfbcd47fd76fd02f07f7a7c5b5
Ying Huang <yhuang@imvu.com>
2018-05-16 22:05:55 -0700

    n

--- install_anaconda3.sh
+++ install_anaconda3.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-bash "/media/volgrp/software/AI/installer/Anaconda3-5.0.1-Linux-x86_64.sh" -u -b
+bash "/media/volgrp/software/AI/installer/Anaconda3-5.1.0-Linux-x86_64.sh" -u -b

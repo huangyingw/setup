c400f46e9530d539bf4856727332e0889a8eb812
Ying Huang <yhuang@imvu.com>
2018-08-25 20:54:11 -0700

    n

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-copyDir="~/myproject/git/AI/setup"
 while read ss
 do
-    #ssh -n $ss "apt-get update && apt-get install realpath"
-    ./copy.sh "$ss" "$copyDir"
-    ssh -n $ss "$copyDir/setup.sh"
+    ssh -n $ss ~/myproject/git/AI/setup/setup.sh
 done < deploy.list

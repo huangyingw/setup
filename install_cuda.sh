a804068696278eae4958bfa0d47e81b62318da13
Ying Huang <yhuang@imvu.com>
2018-11-20 07:53:59 -0800

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,8 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-dpkg -i ./installer/cuda-repo-ubuntu1604-9-2-local_9.2.148-1_amd64.deb
-apt-key add /var/cuda-repo-9-2-local/7fa2af80.pub
+dpkg -i ./installer/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 apt-get update
 apt-get install -y cuda \
-    && rm -fr /var/cuda-repo-9-2-local/
+    && rm -fr /var/cuda-repo-10-0-local-10.0.130-410.48/

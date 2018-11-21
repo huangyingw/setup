266e962faa70dd53d18231e7b14c137105276ba6
Ying Huang <yhuang@imvu.com>
2018-11-21 07:09:38 -0800

    n

--- install_cudnn.sh
+++ install_cudnn.sh
@@ -3,6 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-tar xf ./installer/cudnn-9.2-linux-x64-v7.2.1.38.tgz
+tar xf ./installer/cudnn-10.0-linux-x64-v7.4.1.5.tgz
 cp -v cuda/include/*.* /usr/local/cuda/include/
 cp -v cuda/lib64/*.* /usr/local/cuda/lib64/

3ca113857f3f4883634da0965dd23005c4882d33
Ying Huang <huangyingw@gmail.com>
2020-06-23 09:28:26 -0700

    n

--- install_cudnn.sh
+++ install_cudnn.sh
@@ -3,6 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-tar xf ./installer/cudnn-10.0-linux-x64-v7.4.1.5.tgz
+tar xf ./installer/cudnn-11.0-linux-x64-v8.0.0.180.tgz
 cp -v cuda/include/*.* /usr/local/cuda/include/
 cp -v cuda/lib64/*.* /usr/local/cuda/lib64/

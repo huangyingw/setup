74db728f745a038a7393a98ec488b90e2d6f59c7
Ying Huang <huangyingw@gmail.com>
2020-06-23 12:07:34 -0400

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,8 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-dpkg -i ./installer/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
-apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
-apt-get update
-apt-get install -y cuda \
-    && rm -fr /var/cuda-repo-10-0-local-10.0.130-410.48/
+wget -nc http://developer.download.nvidia.com/compute/cuda/11.0.1/local_installers/cuda_11.0.1_450.36.06_linux.run
+sh cuda_11.0.1_450.36.06_linux.run

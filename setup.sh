ca787d94e0e40e72d10f25305fb18816e815b629
Ying Huang <huangyingw@gmail.com>
2020-08-10 20:04:59 -0700

    n

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ cd "$SCRIPTPATH"
 export CUDA_HOME=/usr/local/cuda
 export PATH="$HOME/anaconda3/bin:$PATH"
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"
-. ~/myproject/git/AI/fastai/fastai/install_fastai.sh
+. ~/myproject/git/AI/fastai/course-v3/install_fastai.sh
 . ./install_kaggle.sh
 . ./configure_jupyter.sh

5c9e9e2fae49e0998ccc4a4c759243a7a389ace6
Ying Huang <huangyingw@gmail.com>
2020-08-14 22:09:32 -0700

    n

--- configure_jupyter.sh
+++ configure_jupyter.sh
@@ -8,3 +8,5 @@ cp -v ./.jupyter/jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_conf
 cp -v ./.jupyter/jupyter_notebook_config.json $HOME/.jupyter/jupyter_notebook_config.json
 mkdir -p $HOME/.jupyter/custom/
 cp -v ./.jupyter/custom/custom.js $HOME/.jupyter/custom/custom.js
+# cp -v ./.jupyter/custom/startup.py $HOME/.jupyter/custom/startup.py
+cp -v ./.ipython/profile_default/startup/disable-warnings.py $HOME/.ipython/profile_default/startup/disable-warnings.py

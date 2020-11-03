bf6e4a59e90a5273000e7dd9d0b57a1dd6b2d1e3
Ying Huang <huangyingw@gmail.com>
2020-11-02 20:04:32 -0800

    n

--- install_nvidia-reload.sh
+++ install_nvidia-reload.sh
@@ -4,3 +4,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 cp -v ./etc/pm/sleep.d/nvidia-reload /etc/pm/sleep.d/nvidia-reload
+cp -v ./lib/systemd/system-sleep/nvidia-reload /lib/systemd/system-sleep/nvidia-reload

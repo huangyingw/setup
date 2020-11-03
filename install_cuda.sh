a89b9f6ddf10bba98ecaa79f53666c1ef5311d82
Ying Huang <huangyingw@gmail.com>
2020-11-02 21:54:50 -0800

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-wget -nc https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run
-sh cuda_11.1.1_455.32.00_linux.run
+wget -nc https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run -P ./installer/ 
+sh ./installer/cuda_11.1.1_455.32.00_linux.run

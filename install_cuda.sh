1175d2b122e41b60a2156a9a4d201cd12e0d75ac
Ying Huang <huangyingw@gmail.com>
2020-11-02 20:54:53 -0800

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -6,6 +6,6 @@ cd "$SCRIPTPATH"
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 apt-get update
 apt-get -y install cuda

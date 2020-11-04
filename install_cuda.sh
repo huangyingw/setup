997456b42ed332e2146c4f86b8febdf586b495fb
Ying Huang <huangyingw@gmail.com>
2020-11-03 16:20:24 -0800

    n

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,5 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-wget -nc https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run -P ./installer/ 
-sh ./installer/cuda_11.1.1_455.32.00_linux.run --silent --toolkit
+wget -nc https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run -P ./installer/
+apt-get -y purge $(ubuntu-drivers devices|awk '/recommended/{print $3}')
+apt autoremove -y
+sh ./installer/cuda_11.1.1_455.32.00_linux.run --driver --silent --toolkit

4254998b98eafc4799f167de740acb3b014fe525
Ying Huang <huangyingw@gmail.com>
2022-01-25 21:41:02 -0800

    n

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -3,6 +3,11 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+if [ $(uname) == "Darwin" ]
+then
+    exit 0
+fi
+
 ./install_gpu.sh
 ./install_cuda.sh
 ./install_cudnn.sh

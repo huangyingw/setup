68b6c2109c95f316052c6655ba8c99b19551947a
Ying Huang <huangyingw@gmail.com>
2022-02-19 20:17:37 -0800

    n

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-. ./install_basic.sh
-. ./install_nvidia.sh
-. ./install_anaconda3.sh
+./install_basic.sh
+./install_nvidia.sh
+./install_anaconda3.sh
 export CUDA_HOME=/usr/local/cuda
 export PATH="$HOME/anaconda3/bin:$PATH"
 export PATH="/usr/local/bin:/opt/local/sbin:$PATH"

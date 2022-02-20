1ecafdc0314b8a418bfa313f1f6fa36561a9ee35
Ying Huang <huangyingw@gmail.com>
2022-02-19 20:17:17 -0800

    n

--- install_anaconda3.sh
+++ install_anaconda3.sh
@@ -3,5 +3,11 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-wget -nc https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -P ./installer/
-bash ./installer/Anaconda3-2020.07-Linux-x86_64.sh -u -b
+if [ $(uname) == "Darwin" ]
+then
+    wget -nc https://repo.anaconda.com/archive/Anaconda3-2021.11-MacOSX-x86_64.sh -P ./installer/
+    bash ./installer/Anaconda3-2021.11-MacOSX-x86_64.sh -u -b
+else
+    wget -nc https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -P ./installer/
+    bash ./installer/Anaconda3-2020.07-Linux-x86_64.sh -u -b
+fi

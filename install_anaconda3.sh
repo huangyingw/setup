996fe100979bcabd1801359e9804e636837a403f
Ying Huang <huangyingw@gmail.com>
2023-07-26 21:19:26 -0700

    n

--- install_anaconda3.sh
+++ install_anaconda3.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-wget -nc https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -P ./installer/
-bash ./installer/Anaconda3-2020.07-Linux-x86_64.sh -u -b
+wget -nc https://repo.anaconda.com/archive/Anaconda3-2023.07-1-Linux-x86_64.sh -P ./installer/
+bash ./installer/Anaconda3-2023.07-1-Linux-x86_64.sh -u -b

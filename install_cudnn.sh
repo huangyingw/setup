ba3bdfcf4b8bf2f22677cb8d0db422bb83bcc89f
Ying Huang <huangyingw@gmail.com>
2023-09-11 15:07:11 -0700

    n

--- install_cudnn.sh
+++ install_cudnn.sh
@@ -1,21 +1,53 @@
 #!/bin/zsh -
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd "$SCRIPTPATH"
 
-cp /var/cudnn-local-repo-ubuntu2204-8.9.4.25/cudnn-local-3C3A81D3-keyring.gpg /usr/share/keyrings/
-dpkg -i cudnn-local-repo-ubuntu2204-8.9.4.25_1.0-1_amd64.deb
+# Get Ubuntu version
+ubuntu_version=$(lsb_release -rs)
 
-search_output=$(apt-cache search libcudnn)
+# Convert Ubuntu version to the format used in the NVIDIA cuDNN Support Matrix
+case "$ubuntu_version" in
+  "18.04")
+    distro="ubuntu1804"
+    ;;
+  "20.04")
+    distro="ubuntu2004"
+    ;;
+  "22.04")
+    distro="ubuntu2204"
+    ;;
+  *)
+    echo "Unsupported Ubuntu version: $ubuntu_version"
+    exit 1
+    ;;
+esac
 
-available_packages=("${(@f)$(echo "$search_output" | awk -F ' - ' '{print $1}')}")
+# Get CUDA version
+cuda_version=$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2-)
 
-for package in "${available_packages[@]}"; do
-    echo "Installing $package..."
-    sudo apt-get install -y "$package"
-    if [[ $? -eq 0 ]]; then
-        echo "Installation of $package succeeded."
-    else
-        echo "Installation of $package failed."
-    fi
-done
+# Convert CUDA version to the format used in the NVIDIA cuDNN Support Matrix
+cuda_version="cuda${cuda_version%.*}"
+
+# Define cuDNN version
+cudnn_version="8.9.4.*"
+
+# Define the architecture
+arch="x86_64"  # Change this to your architecture
+
+# Validate the distro and architecture
+if [[ "$distro/$arch" != "ubuntu1804/x86_64" && "$distro/$arch" != "ubuntu2004/x86_64" && "$distro/$arch" != "ubuntu2204/x86_64" && "$distro/$arch" != "debian11/x86_64" && "$distro/$arch" != "ubuntu2004/sbsa" && "$distro/$arch" != "ubuntu2204/sbsa" ]]; then
+  echo "Invalid distro/arch combination. Please refer to the NVIDIA cuDNN Support Matrix."
+  exit 1
+fi
+
+# Update package list
+sudo apt-get update
+
+# Install cuDNN library
+sudo apt-get install -y libcudnn8="${cudnn_version}-1+${cuda_version}"
+
+# Install cuDNN development package
+sudo apt-get install -y libcudnn8-dev="${cudnn_version}-1+${cuda_version}"
+
+# Install cuDNN samples
+sudo apt-get install -y libcudnn8-samples="${cudnn_version}-1+${cuda_version}"
+
+echo "cuDNN installation completed."

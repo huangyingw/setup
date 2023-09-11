96443010f098fc371ee5f08cdd70526c663d0657
Ying Huang <huangyingw@gmail.com>
2023-09-11 14:42:29 -0700

    n

--- install_cudnn.sh
+++ install_cudnn.sh
@@ -1,8 +1,21 @@
-#!/bin/bash -
+#!/bin/zsh -
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-tar xf ./installer/cudnn-11.1-linux-x64-v8.0.4.30.tgz 
-cp -v cuda/include/*.* /usr/local/cuda/include/
-cp -v cuda/lib64/*.* /usr/local/cuda/lib64/
+cp /var/cudnn-local-repo-ubuntu2204-8.9.4.25/cudnn-local-3C3A81D3-keyring.gpg /usr/share/keyrings/
+dpkg -i cudnn-local-repo-ubuntu2204-8.9.4.25_1.0-1_amd64.deb
+
+search_output=$(apt-cache search libcudnn)
+
+available_packages=("${(@f)$(echo "$search_output" | awk -F ' - ' '{print $1}')}")
+
+for package in "${available_packages[@]}"; do
+    echo "Installing $package..."
+    sudo apt-get install -y "$package"
+    if [[ $? -eq 0 ]]; then
+        echo "Installation of $package succeeded."
+    else
+        echo "Installation of $package failed."
+    fi
+done
